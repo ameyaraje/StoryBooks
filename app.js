9676702c48e0d310491e5aeb4c8fd9bc8ea5e289
ameyaraje <ameyacraje@gmail.com>
2018-09-06 23:06:46 -0500

    added a basic route

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+    res.send('it works');
+});
+
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });

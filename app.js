fdc2844ddbd896d060bcafc7ee1362c7bb1e3c3b
ameyaraje <ameyacraje@gmail.com>
2018-09-11 22:44:52 -0500

    added UI for login

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
-const app = express();
-const passport = require('passport');
+const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const parser = require('cookie-parser');
+const passport = require('passport');
 
-// Load user model
+// Load User Model
 require('./models/User');
 
 // Passport Config
 require('./config/passport')(passport);
-// Load routes
+
+// Load Routes
+const index = require('./routes/index');
 const auth = require('./routes/auth');
 
 // Load Keys
@@ -18,21 +20,26 @@ const keys = require('./config/keys');
 
 // Map global promises
 mongoose.Promise = global.Promise;
-
 // Mongoose Connect
 mongoose.connect(keys.mongoURI, {
-    useMongoClient: true
-}).then(() => console.log('MongoDB Connected')).catch(err => console.log(err));
+  useMongoClient:true
+})
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-app.get('/', (req, res) => {
-    res.send('it works');
-});
+const app = express();
 
-app.use(parser());
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout:'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.use(cookieParser());
 app.use(session({
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: false
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
 }));
 
 // Passport Middleware
@@ -41,12 +48,17 @@ app.use(passport.session());
 
 // Set global vars
 app.use((req, res, next) => {
-    res.locals.user = req.user || null;
-    next();
+  res.locals.user = req.user || null;
+  next();
 });
 
+// Use Routes
+app.use('/', index);
 app.use('/auth', auth);
+
+
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`)
 });
